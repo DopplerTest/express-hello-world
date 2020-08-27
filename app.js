3e3216e826d52bbf3f408d5f989969782f079b3f
Ruud Visser <ruud.visser@doppler.com>
2020-08-26 18:20:53 -0700

    Show Doppler_ variables

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
+const secrets = {};
+for (const key of Object.keys(process.env)) {
+  if (key.startsWith("DOPPLER_") && key !== "DOPPLER_TOKEN") {
+    secrets[key] = process.env[key];
+  }
+}
 
-app.get("/", (req, res) => res.send("Hello from Render!"));
+app.get("/", (req, res) =>
+  res.send(
+    `Hello from Render! Your variables starting with DOPPLER_ are:  ${JSON.stringify(
+      secrets
+    )}`
+  )
+);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
